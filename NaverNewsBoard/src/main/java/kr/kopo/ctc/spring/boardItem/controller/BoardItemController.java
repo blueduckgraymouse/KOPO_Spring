d3f7ba634d0c1753bf6b704e148f20b094e9be22
KOPO_35 <hyunduok@gmail.com>
2022-07-20 14:40:22 +0900

    수정) "/list"의 showBoardList 메서드의 파라미터 cPage 타입 변경 & keword 추가

--- NaverNewsBoard/src/main/java/kr/kopo/ctc/spring/boardItem/controller/BoardItemController.java
+++ NaverNewsBoard/src/main/java/kr/kopo/ctc/spring/boardItem/controller/BoardItemController.java
@@ -12,67 +12,62 @@ import kr.kopo.ctc.spring.boardItem.domain.NewsBoardItem;
 import kr.kopo.ctc.spring.boardItem.dto.PageDto;
 import kr.kopo.ctc.spring.boardItem.service.BoardItemService;
 
-
 @Controller
-@RequestMapping(value="/boardItem")
+@RequestMapping(value = "/boardItem")
 public class BoardItemController {
-	
+
 	@Autowired
 	BoardItemService boardItemService;
 
-	
 	/* 목록 조회 */
-	@RequestMapping(value="/list")
-	public String showBoardList(Model model, @RequestParam(value = "cPage", required = false) String cPage) {
-		PageDto pageDto = boardItemService.getList(cPage);
+	@RequestMapping(value = "/list")
+	public String showBoardList(Model model, 
+			@RequestParam(value = "cPage", required = false, defaultValue = "0") Integer cPage,
+			@RequestParam(value = "keyword", required = false) String keyword) {
+		PageDto pageDto = boardItemService.getList(cPage, keyword);
 		model.addAttribute("PageDto", pageDto);
+		model.addAttribute("keyword", keyword);
 		return "boardItemList";
 	}
 
-	
 	/* 단일 조회 */
-	@RequestMapping(value="/view")
+	@RequestMapping(value = "/view")
 	public String showBoardItem(Model model, @RequestParam(value = "id") int id) {
 		NewsBoardItem boardItem = boardItemService.getView(id);
 		model.addAttribute("boardItem", boardItem);
 		return "boardItemView";
 	}
-	
-	
+
 	/* 입력 양식 호출 */
-	@RequestMapping(value="/insert", method = RequestMethod.GET)
+	@RequestMapping(value = "/insert", method = RequestMethod.GET)
 	public String showInsertForm(Model model) {
 		return "boardItemInsertForm";
 	}
-	
-	
+
 	/* 입력 처리 */
-	@RequestMapping(value="/insert", method = RequestMethod.POST)
+	@RequestMapping(value = "/insert", method = RequestMethod.POST)
 	public String doInsert(Model model, @ModelAttribute NewsBoardItem boardItem) {
 		NewsBoardItem boardItemInserted = boardItemService.insertItem(boardItem);
 		return "redirect:/boardItem/view?id=" + boardItemInserted.getId();
 	}
-	
-	
+
 	/* 수정 양식 호출 */
-	@RequestMapping(value="/update", method = RequestMethod.GET)
+	@RequestMapping(value = "/update", method = RequestMethod.GET)
 	public String showUpdateForm(Model model, @RequestParam(value = "id") int id) {
 		NewsBoardItem boardItem = boardItemService.getView(id);
 		model.addAttribute("boardItem", boardItem);
 		return "boardItemUpdateForm";
 	}
-	
-	
+
 	/* 수정 처리 */
-	@RequestMapping(value="/update", method = RequestMethod.POST)
+	@RequestMapping(value = "/update", method = RequestMethod.POST)
 	public String doUpdate(Model model, @ModelAttribute NewsBoardItem boardItem) {
 		NewsBoardItem boardItemUpdated = boardItemService.updateItem(boardItem);
 		return "redirect:/boardItem/view?id=" + boardItemUpdated.getId();
 	}
-	
-	
+
 	/* 삭제 처리 */
-	@RequestMapping(value="/delete")
+	@RequestMapping(value = "/delete")
 	public String doDelete(Model model, @RequestParam(value = "id") int id) {
 		boardItemService.delete(id);
 		return "redirect:/boardItem/list";
